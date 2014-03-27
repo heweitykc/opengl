531f9fbee7a6baa67d1561d5d76afa9c8d84930a
callee <heweitykc@163.com>
2014-03-27 23:25:33 +0800

    opengl

--- sample2/sample2/main.cpp
+++ sample2/sample2/main.cpp
@@ -3,6 +3,13 @@
 void RenderScene()
 {
 	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0, 1.0, 0.0);
+	glBegin(GL_POLYGON);
+		glVertex3f(0.25,0.25,0.0);
+		glVertex3f(0.75,0.25,0.0);
+		glVertex3f(0.75,0.75,0.0);
+		glVertex3f(0.25,0.75,0.0);
+	glEnd();
 	glFlush();
 }
 
@@ -10,6 +17,9 @@ void RenderScene()
 void SetupRC()
 {
 	glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
 }
 
 void main()
@@ -17,8 +27,6 @@ void main()
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutCreateWindow("Simple");
 	glutDisplayFunc(RenderScene);
-
 	SetupRC();
-
 	glutMainLoop();
 }
