1fedb8cbfbaefb30bdbca5357dd987bccb97329d
callee <heweitykc@163.com>
2014-03-28 10:57:11 +0800

    222

--- src/build.bat
+++ src/build.bat
@@ -1,2 +1,21 @@
-cl main.cpp	
-pause
+echo off
+cl main.c /I ../libs/glut/include /MTd /TC /link ../libs/glut/lib/glut.lib ../libs/glut/lib/glut32.lib
+@if ERRORLEVEL == 0 (
+   goto good
+)
+
+@if ERRORLEVEL != 0 (
+   goto bad
+)
+
+:good
+   echo "clean compile"
+   echo %ERRORLEVEL%
+   goto end
+
+:bad
+   echo "error or warning"
+   echo %ERRORLEVEL%
+   goto end
+
+:end
